42c7e1c2abb53b23e4cc01b444fe938e5020679e
Johannes Goebel <GoebelJohannes@gmx.net>
2024-05-08 02:00:10 +0200

    Begin integrating TaskInterpreter

--- src/main/java/com/github/appreciated/llm_project_quickstart_plugin/GenerateTopDownAction.java
+++ src/main/java/com/github/appreciated/llm_project_quickstart_plugin/GenerateTopDownAction.java
@@ -5,6 +5,7 @@ import com.github.appreciated.llm.ChatGptCommunicationService;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.DialogBuilder;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -12,6 +13,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 
 public class GenerateTopDownAction extends AnAction {
 
@@ -23,12 +25,12 @@ public class GenerateTopDownAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         AppSettingsState instance = AppSettingsState.getInstance();
-        if (instance.openAiToken == null || instance.openAiToken.isEmpty()){
+        if (instance.openAiToken == null || instance.openAiToken.isEmpty()) {
             DialogBuilder dialogBuilder = new DialogBuilder(e.getProject());
             dialogBuilder.setTitle("Missing Open AI API Token");
             JPanel dialogPanel = new JPanel(new BorderLayout());
             JLabel label = new JLabel("Please add a Open AI API Token under Settings/Tools/LLM Quick Start");
-            label.setPreferredSize(new Dimension(130,45));
+            label.setPreferredSize(new Dimension(130, 45));
             dialogPanel.add(label, BorderLayout.CENTER);
             dialogBuilder.setCenterPanel(dialogPanel);
             dialogBuilder.setOkActionEnabled(false);
@@ -39,9 +41,18 @@ public class GenerateTopDownAction extends AnAction {
                 try (InputStream resourceAsStream = getClass().getResourceAsStream("/Tasks.yaml")) {
                     assert resourceAsStream != null;
                     final VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-                    taskInterpreter.start(new String(resourceAsStream.readAllBytes()), virtualFile.toNioPath());
+                    Path nioPath = virtualFile.toNioPath();
+                    String yamlInput = new String(resourceAsStream.readAllBytes());
+                    ApplicationManager.getApplication()
+                            .executeOnPooledThread(() -> {
+                                try {
+                                    taskInterpreter.start(yamlInput, nioPath);
+                                } catch (IOException | InterruptedException ex) {
+                                    throw new RuntimeException(ex);
+                                }
+                            });
                 }
-            } catch (IOException | InterruptedException ex) {
+            } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         }
