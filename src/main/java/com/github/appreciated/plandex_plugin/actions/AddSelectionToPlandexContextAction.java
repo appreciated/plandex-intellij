d2a6d5cb436972894d415e639e18b8edf6b4508f
Johannes Goebel <GoebelJohannes@gmx.net>
2024-06-12 01:22:01 +0200

    Added support for folders

--- src/main/java/com/github/appreciated/plandex_plugin/actions/AddSelectionToPlandexContextAction.java
+++ src/main/java/com/github/appreciated/plandex_plugin/actions/AddSelectionToPlandexContextAction.java
@@ -14,8 +14,17 @@ public class AddSelectionToPlandexContextAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         VirtualFile selectedFile = (VirtualFile) e.getDataContext().getData("virtualFile");
 
+        if (selectedFile == null) {
+            return; // oder eine geeignete Fehlerbehandlung
+        }
+
         String modulePath = FileUtil.getCurrentModulePathFromProject(e.getProject(), selectedFile);
         String relativePath = selectedFile.getPath().replaceFirst(modulePath + "/", "");
+
+        if (selectedFile.isDirectory()) {
+            relativePath += " --recursive";
+        }
+
         String command = "pdx load " + relativePath;
         executeCommandInTerminal(e.getProject(), selectedFile, command);
     }
