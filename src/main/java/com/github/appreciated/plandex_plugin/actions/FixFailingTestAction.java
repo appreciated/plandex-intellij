9e8858bd1879f548a1233402ccbcdb18831bce86
Johannes Goebel <GoebelJohannes@gmx.net>
2024-07-03 23:48:36 +0200

    Fix failing test

--- src/main/java/com/github/appreciated/plandex_plugin/actions/FixFailingTestAction.java
+++ src/main/java/com/github/appreciated/plandex_plugin/actions/FixFailingTestAction.java
@@ -2,20 +2,29 @@ package com.github.appreciated.plandex_plugin.actions;
 
 import com.github.appreciated.plandex_plugin.util.FileUtil;
 import com.intellij.execution.TestStateStorage;
+import com.intellij.execution.testDiscovery.IntellijTestDiscoveryProducer;
 import com.intellij.execution.testframework.JavaTestLocator;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.execution.testframework.TestTreeView;
+import com.intellij.execution.testframework.sm.runner.SMTestProxy;
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerTestTreeView;
+import com.intellij.execution.testframework.sm.runner.ui.SMTestRunnerResultsForm;
+import com.intellij.execution.ui.layout.impl.RunnerContentUi;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import javax.swing.tree.TreePath;
 import java.io.IOException;
 import java.util.List;
 
@@ -38,22 +47,33 @@ public class FixFailingTestAction extends AnAction {
         if (project != null) {
             PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
             PsiMethod element = (PsiMethod) e.getData(CommonDataKeys.PSI_ELEMENT);
+            if (file == null || element == null) {
+                Messages.showErrorDialog(project, "No file or method selected.", "Error");
+                return;
+            }
+
             TestStateStorage instance = TestStateStorage.getInstance(project);
             PsiClass parent = (PsiClass) element.getParent();
             String url = JavaTestLocator.createLocationUrl(JavaTestLocator.TEST_PROTOCOL, parent.getQualifiedName(), element.getName());
+
             TestStateStorage.Record state = instance.getState(url);
+            Content[] runnerContents = (Content[]) e.getDataContext().getData("runnerContents");
+            SMTestRunnerResultsForm component = (SMTestRunnerResultsForm) runnerContents[0].getComponent();
+            SMTRunnerTestTreeView treeView = (SMTRunnerTestTreeView) component.getTreeView();
+            SMTestProxy selectedTest = treeView.getSelectedTest(treeView.getSelectionPath());
+            String stackTrace = selectedTest.getStacktrace();
+
             if (state != null) {
                 ApplicationManager.getApplication().executeOnPooledThread(() -> {
                     VirtualFile virtualFile = file.getVirtualFile();
                     if (virtualFile != null) {
-
                         String modulePath = FileUtil.getCurrentModulePathFromProject(e.getProject(), virtualFile);
                         try {
                             sendClear(e.getProject(), modulePath);
                             executeCommandForEachFileInTerminal(e.getProject(), List.of(file.getVirtualFile()), "plandex load", "", modulePath, true);
                             executeCommandInTerminal(
                                     e.getProject(),
-                                    "plandex tell \"Make a change to the method %s in the class %s. <Your Prompty>\"".formatted(getPsiMethodName(element), modulePath),
+                                    "plandex tell \"Fix the test method %s in the class %s. The stacktrace is as follows: %s\"".formatted(getPsiMethodName(element), modulePath, stackTrace),
                                     modulePath,
                                     false
                             );
@@ -67,6 +87,8 @@ public class FixFailingTestAction extends AnAction {
             } else {
                 Messages.showMessageDialog(project, "No failing tests found.", "Info", Messages.getInformationIcon());
             }
+        } else {
+            Messages.showErrorDialog("Project is not available.", "Error");
         }
     }
 }
